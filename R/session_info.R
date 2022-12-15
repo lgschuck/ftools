47f878d1282454bc3d1c4e3530664a55c0e4ac52
Luís Gustavo Schuck <luisgustavoschuck@yahoo.com.br>
2022-12-14 21:19:56 -0300

    updates session_info

--- R/session_info.R
+++ R/session_info.R
@@ -6,34 +6,32 @@
 #'
 session_info <- function(){
   r_version <- getRversion()
+  system <- sessionInfo()$running
   user <- Sys.getenv('USERNAME')
   machine <- Sys.getenv('COMPUTERNAME')
   time <- format(Sys.time(), "%Y/%m/%d - %X")
   libs <- .libPaths()
   env_obj <- ls(all.names = T, envir = globalenv())
-  search <- search()
-  pacotes <- installed.packages()
 
   header <- "\n=========================================\n"
 
   writeLines(paste0(header,
                     "R Version:\t",
                     r_version,
+                    "\nSystem:\t",
+                    system,
                     "\nUser:\t",
                     user,
                     "\nMachine:\t",
                     machine,
                     "\nTime:\t",
-                    time,
+                    time, "\n",
                     header,
                     "Libraries:\n\t",
                     paste(libs, collapse = "\n\t"),
                     header,
                     "Global Env Objects:\n\t",
                     paste(env_obj, collapse = "\n\t"),
-                    header,
-                    "Search:\n\t",
-                    paste(search, collapse = "\n\t"),
                     header
                     ))
 
