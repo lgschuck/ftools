f0fead9d6bac22fd1a1c384a34fe15eed3e9de9e
Luís Gustavo Schuck <luisgustavoschuck@yahoo.com.br>
2022-12-14 21:49:27 -0300

    write_log function created

---
+++ R/write_log.R
@@ -0,0 +1,13 @@
+#' Function for write messages in text file
+#' @export write_log
+#'
+#' @param file log file
+#' @param message message to be saved in the log file
+#'
+write_log <- function(message = '', file = 'log.txt'){
+  message <- paste(format(Sys.time(), "%Y%m%d %X"),
+             '|',Sys.getenv('USERNAME'),
+             '| ', message)
+  write(message, file = file, append = T)
+
+}
