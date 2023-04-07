2d060b413b61bb00833ecfdb57d68ba3be649ce3
lgschuck <luisgustavoschuck@yahoo.com.br>
2023-04-07 02:25:09 -0300

    Improve session_info and write_log.

--- R/session_info.R
+++ R/session_info.R
@@ -4,35 +4,39 @@
 #' @examples
 #' session_info()
 #'
-session_info <- function(){
-  r_version <- getRversion()
-  system <- sessionInfo()$running
-  user <- Sys.getenv('USERNAME')
-  machine <- Sys.getenv('COMPUTERNAME')
-  time <- format(Sys.time(), "%Y/%m/%d - %X")
-  libs <- .libPaths()
-  env_obj <- ls(all.names = T, envir = globalenv())
+session_info <- function(r_version = getRversion(),
+                         system = osVersion,
+                         user = Sys.getenv('USERNAME'),
+                         machine = Sys.getenv('COMPUTERNAME'),
+                         time = format(Sys.time(), "%Y/%m/%d - %X"),
+                         libs = .libPaths(),
+                         env_obj = ls(all.names = T, envir = globalenv()),
+                         header = paste0("\n", paste0(rep('=', 60), collapse = ''), "\n"),
+                         limit = 12) {
+  space <- '         '
+  limit <- min(limit, 12)
 
-  header <- "\n=========================================\n"
-
-  writeLines(paste0(header,
-                    "R Version: \t",
-                    r_version,
-                    "\nSystem:\t",
-                    system,
-                    "\nUser:\t",
-                    user,
-                    "\nMachine:\t",
-                    machine,
-                    "\nTime:\t",
-                    time,
-                    header,
-                    "Libraries:\n\t",
-                    paste(libs, collapse = "\n\t"),
-                    header,
-                    "Global Env Objects:\n\t",
-                    paste(env_obj, collapse = "\n\t"),
-                    header
-                    ))
+  writeLines(
+    paste0(
+      header,
+      substr(paste0("R Version: ", space), 1, limit - 1),
+      r_version,
+      substr(paste0("\nSystem: ", space), 1, limit),
+      system,
+      substr(paste0("\nUser: ", space), 1, limit),
+      user,
+      substr(paste0("\nMachine:", space), 1, limit),
+      machine,
+      substr(paste0("\nTime:", space), 1, limit),
+      time,
+      header,
+      "Libraries:\n\t",
+      paste(libs, collapse = "\n\t"),
+      header,
+      "Global Env Objects:\n\t",
+      paste(env_obj, collapse = "\n\t"),
+      header
+    )
+  )
 
 }
--- R/write_log.R
+++ R/write_log.R
@@ -4,10 +4,18 @@
 #' @param file log file
 #' @param message message to be saved in the log file
 #'
-write_log <- function(message = '', file = 'log.txt'){
-  message <- paste(format(Sys.time(), "%Y%m%d %X"),
-             '|',Sys.getenv('USERNAME'),
-             '| ', message)
+#' #' @examples
+#' write_log('A Simple Test.')
+#' readLines("log.txt")
+#'
+#'
+write_log <- function(message = '', file = 'log.txt') {
+  message <- paste(format(Sys.time(), "%Y-%m-%d %X"),
+                   '|',
+                   Sys.getenv('USERNAME'),
+                   '| ',
+                   message,
+                   '|')
   write(message, file = file, append = T)
 
 }
