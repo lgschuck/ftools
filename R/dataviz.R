f238babb1d9c1b7b9ad7d173ffdbea64f01cb62d
Luís Gustavo Schuck <luisgustavoschuck@yahoo.com.br>
2024-12-08 01:19:11 -0300

    Update on dataviz (better layout and test of bind cache)

--- R/dataviz.R
+++ R/dataviz.R
@@ -6,18 +6,17 @@
 #' @param dataset dataset for analysis
 #'
 #' @examples
-#' if(FALSE) dataviz(mtcars)
+#' if(interactive()) dataviz(mtcars)
 #'
 #' @export
 #'
-#' @importFrom data.table setDT
-#'
 #' @rawNamespace import(shiny, except = c(dataTableOutput, renderDataTable))
 #'
-#' @importFrom DT datatable formatCurrency DTOutput renderDT
-#' @import dplyr
+#' @import data.table
+#' @importFrom dplyr select filter mutate pull
 #' @import bslib
 #' @import bsicons
+#' @import DT
 #' @importFrom graphics abline hist
 #' @importFrom stats median
 #' @importFrom utils object.size
@@ -25,6 +24,7 @@
 dataviz <- function(dataset){
 
   stopifnot(is.data.frame(dataset))
+  dataset_classes <- class(dataset)
   setDT(dataset)
 
   var_num <- names(dataset[, sapply(dataset, is.numeric), with = FALSE])
@@ -32,66 +32,103 @@ dataviz <- function(dataset){
   # close browser tab
   js_exit <- "Shiny.addCustomMessageHandler('closeWindow', function(m) {window.close();});"
 
+  main_box_values <- layout_columns(
+    height = "15%",
+    col_widths = c(3, 3, 3, 3),
+    value_box(
+      title = "Dataset Class",
+      value = dataset_classes[1],
+      showcase = bs_icon("file-binary", class = "custom-icon"),
+      theme = "primary"
+    ),
+    value_box(
+      title = "Rows",
+      value = nrow(dataset) |> format(big.mark = ','),
+      showcase = bs_icon("layout-text-sidebar-reverse"),
+      theme = "primary"
+    ),
+    value_box(
+      title = "Columns",
+      value = ncol(dataset) |> format(big.mark = ','),
+      showcase = bs_icon("layout-three-columns"),
+      theme = "primary"
+    ),
+    # value_box(
+    #   title = "Columns with NA's",
+    #   value = sum(colSums(is.na(dataset)) > 0),
+    #   showcase = bs_icon("question-circle"),
+    #   theme = "primary"
+    # ),
+    value_box(
+      title = "Size (MB)",
+      value = (object.size(dataset)/1e6) |> as.numeric() |> round(2),
+      showcase = bs_icon("sd-card"),
+      theme = "primary"
+    ),
+  )
+
   ui <- page_navbar(
     id = 'navbar',
     theme = bs_theme(
-      bg = "#e6e5e3",
+      bg = "#f2f2f2",
       fg = "#000",
       primary = "#0072B2",
-      secondary = "#00aaB2"
-      # success = "#009E73"
+      secondary = "#00aaB2",
+      success = "#009E73"
     ),
-    title = "ftools - Data Viz",
-    bg = '#536b8f',
+    title = "Banritools - Data Viz",
+    bg = '#09679c',
 
-    nav_panel("Summary",
-              mainPanel(
-                layout_column_wrap(
-                  width = 1/3,
-                  value_box(
-                    title = "Rows",
-                    value = nrow(dataset),
-                    showcase = bs_icon("layout-text-sidebar-reverse"),
-                    theme = "teal"
-                  ),
-                  value_box(
-                    title = "Columns",
-                    value = ncol(dataset),
-                    showcase = bs_icon("layout-three-columns"),
-                    theme = "teal"
-                  ),
-                  value_box(
-                    title = "Size (MB)",
-                    value = object.size(dataset)/1e6,
-                    showcase = bs_icon("sd-card"),
-                    theme = "teal"
-                  )),
-                card(full_screen = TRUE, max_height = 1200,
-                     card_header(class = "bg-dark", "Variables"),
-                     card_body(DTOutput('summary_t1'))
-                ),
-              )),
-    nav_panel("Numeric",
-              mainPanel(
-                div(
-                  fluidRow(
-                    column(2, navlistPanel(
-                      widths = c(12, 12), "Parameters",
-                      tabPanel(selectInput('plots_sel_vars', 'Variable', choices = var_num )),
-                      tabPanel(numericInput('plots_bins', 'Bins',
-                                            value = 10, min = 5, step = 5)),
-                      tabPanel(numericInput('plots_digits', 'Digits',
-                                            value = 2, min = 0, max = 9, step = 1)),
-                      tabPanel(numericInput('plots_percentile', 'Percentile',
-                                            value = 50, min = 0, max = 100, step = 5))
-                    )),
-                    column(8, plotOutput('plots_g1')),
-                    column(2, DTOutput('plots_t1')),
-                  )),
-                style = 'width:100%;')
+    # useBusyIndicators(),
+    # page summary ------------------------------------------------------------
+    nav_panel("Summary", icon = bs_icon("info-square-fill"),
+              useBusyIndicators(),
+              main_box_values,
+              card(full_screen = T, height = '625px',
+                    card_header(class = "bg-primary", "Variables"),
+                    card_body(DTOutput('summary_t1'))
+              ),
     ),
-    nav_panel(value = 'exit', title = "Exit",
-              # exit js script
+
+    # page numeric ------------------------------------------------------------
+    nav_panel("Numemic", icon = bs_icon("bar-chart-fill"),
+      main_box_values,
+      card(full_screen = T, card_body(
+      layout_columns(col_widths = c(2, 7, 3), #row_heights = 600,
+         card(card_header('Parameters', class = "bg-primary"),
+              card_body(
+                selectInput('plots_sel_vars', 'Variable', choices = var_num),
+                selectInput('plots_sel_vars2', 'Variable 2', choices = var_num),
+                numericInput('plots_percentile', 'Percentile',
+                             value = 50, min = 0, max = 100, step = 5),
+                checkboxInput('plots_outliers', 'Remove Outliers', value = F)
+              )),
+         navset_card_tab(#height = 600,
+           full_screen = T,
+           nav_panel("Histogram", full_screen = T,
+                     card_body(plotOutput('plots_g1')),
+                     card_footer(numericInput('plots_bins', 'Bins',
+                                              value = 10, min = 5, step = 5),)),
+           nav_panel("Boxplot", full_screen = T,
+                     card_body(plotOutput('plots_g2'))),
+           nav_panel("Scatter", full_screen = T,
+                     card_body(plotOutput('plots_g3')),
+                     card_footer(checkboxInput('plots_scatter_lm', 'Linear Model', value = F))
+           ),
+           nav_panel("Linear Model", full_screen = T,
+                     verbatimTextOutput("plots_linear_model")),
+         ),
+         card(full_screen = T,
+              card_header('Statistics', class = "bg-primary"),
+              card_body(DTOutput('plots_t1')),
+              card_footer(numericInput('plots_digits', 'Digits',
+                                       value = 2, min = 0, max = 9, step = 1)))
+      ))),
+    ),
+
+    # page exit ---------------------------------------------------------------
+    nav_spacer(),
+    nav_panel(value = 'exit', title = "Exit", icon = bs_icon('x-square-fill'),
               tags$head(tags$script(HTML(js_exit))))
   )
 
@@ -108,55 +145,132 @@ dataviz <- function(dataset){
           var = names(df$df),
           type = lapply(as.list(df$df), typeof) |> unlist(),
           class = lapply(as.list(df$df),
-                          \(x) class(x) |> paste(collapse = '/')) |> unlist(),
-          size = (lapply(as.list(df$df), object.size) |> unlist())/1e3
+                         \(x) class(x) |> paste(collapse = '/')) |> unlist(),
+          size = (lapply(as.list(df$df), object.size) |> unlist())/1e3,
+          min = lapply(as.list(df$df), \(x) if(is.numeric(x)) mina(x) else NA) |> unlist(),
+          max = lapply(as.list(df$df), \(x) if(is.numeric(x)) mana(x) else NA) |> unlist(),
+          n_nas = sapply(df$df, \(x) length(x[is.na(x)])),
+          nas = sapply(df$df, \(x) length(x[is.na(x)]))/sapply(df$df, length)
         ),
-        options = list(dom = 'Bpf', pageLength = 10),
-        rownames = F, colnames = c('Variable', 'type', 'class', 'Size (kB)'))
+        extensions = 'ColReorder',
+        options = list(dom = 'Bftp', pageLength = 6, colReorder = T),
+        rownames = F, colnames = c('Variable', 'Type', 'Class', 'Size (kB)',
+                                   'Min', 'Max', "NA's", "% NA's"))
     )
 
     output$summary_t1 <- renderDT(
       summary_t1() |>
-        formatCurrency('size', digits = 2, currency = '')
+        formatCurrency(c('size', 'min', 'max', 'n_nas'), digits = 2, currency = '') |>
+        formatPercentage('nas', digits = 2)
     )
 
     # numeric page events -----------------------------------------------------
     df_plots <- reactive(
-      df$df_num |>
-        select(input$plots_sel_vars)
-    )
+      {
+        df_plots_temp <- df$df_num |>
+          select(input$plots_sel_vars, input$plots_sel_vars2)
 
-    value_plots <- reactive(df_plots() |> pull(input$plots_sel_vars))
+        if(input$plots_outliers){
+          q1 <- p25(df_plots_temp |> pull(input$plots_sel_vars))
+          q3 <- p75(df_plots_temp |> pull(input$plots_sel_vars))
+
+          interquatile <- q3 - q1
+
+          df_plots_temp <- df_plots_temp[get(input$plots_sel_vars) >= (q1 - 1.5 * interquatile) &
+                                           get(input$plots_sel_vars) <= (q3 + 1.5 * interquatile), ]
+
+        }
+
+        df_plots_temp
+      }
+    )  |>
+      bindCache(input$plots_sel_vars, input$plots_sel_vars2, input$plots_outliers)
+
+    plots_var <- reactive(df_plots() |> pull(input$plots_sel_vars))
+    plots_var2 <- reactive(df_plots() |> pull(input$plots_sel_vars2))
 
     output$plots_g1 <- renderPlot(
       {
-        hist(value_plots(), col = 'deepskyblue2', breaks = input$plots_bins,
-             main = 'Histogram', xlab = '', ylab = 'Count')
-        abline(v = pn(value_plots(), input$plots_percentile/100), col = 'red')
+        hist(plots_var(), col = 'deepskyblue', breaks = input$plots_bins,
+             main = '', xlab = '', ylab = 'Count')
+        abline(v = pn(plots_var(), input$plots_percentile/100), col = 'red')
       }
-
     )
+
+    output$plots_g2 <- renderPlot(
+      {
+        boxplot(plots_var(), horizontal = T, col = 'deepskyblue')
+        abline(v = pn(plots_var(), input$plots_percentile/100), col = 'red')
+      }
+    )
+
+    lm_sample <- reactive(
+      sample.int(length(plots_var()),
+                 min(15e3, length(plots_var())), replace = F) |>
+        sort()
+    )
+
+    plots_linear_model <- reactive(
+      if(input$plots_scatter_lm){
+
+        plots_x <- plots_var2()[lm_sample()]
+        plots_y <- plots_var()[lm_sample()]
+        lm(plots_y ~ plots_x)
+
+      })
+
+    output$plots_linear_model <- renderPrint({ summary(plots_linear_model()) })
+
+    output$plots_g3 <- renderPlot(
+      {
+        plot(plots_var2(), plots_var(),
+             type = 'p', col = 'deepskyblue',
+             xlab = input$plots_sel_vars2,
+             ylab = input$plots_sel_vars)
+        if(input$plots_scatter_lm){
+          lines(plots_var2()[lm_sample()],
+                plots_linear_model()$fitted.values,
+                col = 'brown3')
+        }
+      }
+    )
+
+    # metrics -----------------------------------------------------------------
+    plots_metrics_obs <- reactive(length(plots_var()))
+    plots_metrics_n_nas <- reactive(length(plots_var()[is.na(plots_var())]))
+    plots_metrics_min <- reactive(mina(plots_var()))
+    plots_metrics_q1 <- reactive(pn(plots_var(), 0.25))
+    plots_metrics_median <- reactive(median(plots_var(), na.rm = T))
+    plots_metrics_mean <- reactive(mean(plots_var(), na.rm = T))
+    plots_metrics_q3 <- reactive(pn(plots_var(), 0.75))
+    plots_metrics_max <- reactive(mana(plots_var()))
+    plots_metrics_percentile <- reactive(pn(plots_var(), input$plots_percentile/100))
+    plots_metrics_sd <- reactive(sd(plots_var(), na.rm = T))
+    plots_metrics_correlation <- reactive(cor(plots_var(), plots_var2(),
+                                              method = 'p', use = "na.or.complete"))
 
     plots_t1 <- reactive(
       datatable(
         data.frame(
-          var = c('Minimum', 'Percentile 5', 'Percentile 25', 'Median', 'Mean',
-                  'Percentile 75', 'Percentile 95', 'Maximum',
+          var = c(paste("% NA's (", plots_metrics_n_nas(), '/',
+                        plots_metrics_obs(), ')'),
+                  'Minimum',
+                  'Percentile 25', 'Median', 'Mean', 'Percentile 75', 'Maximum',
                   paste('Percentile', input$plots_percentile),
-                  'Number of NAs'),
-          value = c(mina(value_plots()),
-                    pn(value_plots(), 0.05),
-                    pn(value_plots(), 0.25),
-                    median(value_plots(), na.rm = T),
-                    mean(value_plots(), na.rm = T),
-                    pn(value_plots(), 0.75),
-                    pn(value_plots(), 0.95),
-                    mana(value_plots()),
-                    pn(value_plots(), input$plots_percentile/100),
-                    length(value_plots()[is.na(value_plots())])
+                  'Standard Deviation', 'Pearson Correlation'),
+          value = c(plots_metrics_n_nas()/plots_metrics_obs() * 100,
+                    plots_metrics_min(),
+                    plots_metrics_q1(),
+                    plots_metrics_median(),
+                    plots_metrics_mean(),
+                    plots_metrics_q3(),
+                    plots_metrics_max(),
+                    plots_metrics_percentile(),
+                    plots_metrics_sd(),
+                    plots_metrics_correlation()
           )
         ),
-        options = list(dom = 'B', pageLength = 20),
+        options = list(dom = 'B', pageLength = 20, ordering = F),
         rownames = F, colnames = NULL))
 
     output$plots_t1 <- renderDT(
